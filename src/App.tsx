0d9c122cbe4be5c487955e17f507e1865e23440d
Zeha Irawan <zehairawan@gmail.com>
2023-05-27 14:48:43 +0800

    Mock searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,47 @@
-import { TextField } from "@mui/material";
+import { Box, TextField, Button, Typography } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+  const [usernameQuery, setUsernameQuery] = useState<string>("");
+
+  const [users, setUsers] = useState<{ username: string }[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsernameQuery(e.target.value);
+    setUsers([]);
+  };
+
+  const handleClick = () => {
+    setUsers([{ username: "Zeha" }, { username: "Raj" }]);
+  };
+
   return (
-    <>
-      <TextField placeholder="Search here" />
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "400px",
+        gap: "16px",
+      }}
+    >
+      <TextField
+        value={usernameQuery}
+        onChange={handleChange}
+        placeholder="Enter Username"
+      />
+      <Button onClick={handleClick} variant="contained">
+        Search
+      </Button>
+
+      {usernameQuery && users.length > 0 && (
+        <>
+          <Typography variant="body1">{`Showing users for "${usernameQuery}"`}</Typography>
+          {users.map((user) => {
+            return <Typography variant="body1">{user.username}</Typography>;
+          })}
+        </>
+      )}
+    </Box>
   );
 }
 
